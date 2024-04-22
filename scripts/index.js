d97bc210109c1533287176970b3e33f55e4abdd1
Brandon King <brandonking25@gmail.com>
2024-04-22 01:38:43 -0400

    Completed card functionality updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-// Card Array
+// Initial card array
 const initialCards = [
   {
     name: "Yosemite Valley",
@@ -26,7 +26,7 @@ const initialCards = [
   },
 ];
 
-// Selector variables
+// Profile and profile modal selector variables
 const openProfileButton = document.querySelector(".profile__edit-button");
 const closeProfileButton = document.querySelector(".modal__close");
 const profileName = document.querySelector(".profile__title");
@@ -35,41 +35,51 @@ const inputName = document.querySelector("#modal__input-name");
 const inputDescription = document.querySelector("#modal__input-description");
 const modal = document.querySelector(".modal");
 const modalForm = document.querySelector(".modal__form");
-const cardTemplate = document.querySelector("#card-template").content;
+// Card template variables
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 const cardContainer = document.querySelector(".card__container");
+// Add new card modal variables
 const openCardButton = document.querySelector(".profile__add-button");
 const closeCardButton = document.querySelector(".modal__add-close");
 const modalCardForm = document.querySelector(".modal__card-form");
 const cardPlaceName = document.querySelector("#modal__input-place");
 const cardPlaceURL = document.querySelector("#modal__input-url");
 const modalCard = document.querySelector(".modal__card-form");
+// Image preview variables
 const previewImageModal = document.querySelector(".modal__preview-wrapper");
+const previewImage = document.querySelector(".modal__preview-image");
+const previewImageClose = document.querySelector(".modal__preview-close");
+const previewImageCaption = document.querySelector(".modal__preview-caption");
 
-// Dynamic iterative card creation loop
+// Card creation loop for initial card array
 initialCards.forEach((item) => {
   const createCard = createCardElement(item);
   cardContainer.append(createCard);
 });
 
-// Functions
+// Card creation functions for user-added content
 function createCardElement(item) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImage = cardElement.querySelector(".card__image");
   const cardName = cardElement.querySelector(".card__title");
-  const trashButton = cardElement.querySelector(".card__trash-button");
   const likeButton = cardElement.querySelector(".card__like-button");
-  trashButton.addEventListener("click", () => {
-    cardElement.remove();
-  });
+  const deleteButton = cardElement.querySelector(".card__delete-button");
   likeButton.addEventListener("click", () => {
     likeButton.classList.toggle("card__like-button_active");
   });
-  cardImage.addEventListener("click", () => {
-    previewImageModal.classList.add("modal__preview-image_active");
+  deleteButton.addEventListener("click", () => {
+    cardElement.remove();
   });
   cardName.textContent = item.name;
   cardImage.src = item.link;
   cardImage.alt = item.name;
+  cardImage.addEventListener("click", () => {
+    togglePreviewImage();
+    previewImage.src = cardImage.src;
+    previewImage.alt = cardImage.alt;
+    previewImageCaption.textContent = cardImage.alt;
+  });
   return cardElement;
 }
 
@@ -83,6 +93,11 @@ function handleCardSubmit(evt) {
   toggleCardModal();
 }
 
+function toggleCardModal() {
+  modalCard.classList.toggle("modal_opened");
+}
+
+// Profile editing functions
 function openProfileModal() {
   inputName.value = profileName.textContent;
   inputDescription.value = profileDescription.textContent;
@@ -100,8 +115,8 @@ function toggleProfileModal() {
   modal.classList.toggle("modal_opened");
 }
 
-function toggleCardModal() {
-  modalCard.classList.toggle("modal_opened");
+function togglePreviewImage() {
+  previewImageModal.classList.toggle("modal__preview-image_active");
 }
 
 // Event Listeners
@@ -111,3 +126,4 @@ modalForm.addEventListener("submit", handleFormSubmit);
 openCardButton.addEventListener("click", toggleCardModal);
 closeCardButton.addEventListener("click", toggleCardModal);
 modalCardForm.addEventListener("submit", handleCardSubmit);
+previewImageClose.addEventListener("click", togglePreviewImage);
